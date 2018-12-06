d72a6d6cb1b85fb9bd56ed238be718a4d688b371
CVAS7 <suresh.malasani@gmail.com>
2018-12-06 20:14:28 +0400

    Create Add_Sub_Funcation_Day2.py

---
+++ Add_Sub_Funcation_Day2.py
@@ -0,0 +1,19 @@
+def add(n1,n2):
+    add1 = n1+n2
+    return add1
+
+def sub(n3,n4):
+    sub1 = n3-n4
+    return sub1
+
+#Main here
+Count1 = 10
+Count2 = 20
+Eng1 = 200
+Eng2 = 150
+Ind1 = 300
+addres = add(Eng1,Eng2)
+print addres
+
+subres = sub(addres,Ind1)
+print subres+1
