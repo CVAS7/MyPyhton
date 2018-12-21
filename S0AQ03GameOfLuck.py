38e3d90593f2b3930b43574a3424d9766799b05e
CVAS7 <suresh.malasani@gmail.com>
2018-12-21 04:17:08 +0400

    Create S0AQ03GameOfLuck.py

---
+++ S0AQ03GameOfLuck.py
@@ -0,0 +1,48 @@
+"""
+You invite home 10 of your closest friends and play a simple game of luck.
+Each of your friend picks up a card from a stack of cards numbered from 300 to 325.
+The person with the highest number wins
+Simulate this simple game using Python
+[ Hint : Make sure that no 2 friends get the same numbered card ]
+"""
+import random
+a = xrange(300,326)
+#for i in a:
+#    print i
+f,p = 0,0
+C10Cards = random.sample(a,10)
+print C10Cards
+
+for i in C10Cards:
+#    print i
+    if i > f:
+        p = C10Cards.index(i)
+        f = i
+  
+print "Friend in place,",p+1," Wins with",f,"as Card Numbers"
+
+========================
+Output:
+"""
+You invite home 10 of your closest friends and play a simple game of luck.
+Each of your friend picks up a card from a stack of cards numbered from 300 to 325.
+The person with the highest number wins
+Simulate this simple game using Python
+[ Hint : Make sure that no 2 friends get the same numbered card ]
+"""
+import random
+a = xrange(300,326)
+#for i in a:
+#    print i
+f,p = 0,0
+C10Cards = random.sample(a,10)
+print C10Cards
+
+for i in C10Cards:
+#    print i
+    if i > f:
+        p = C10Cards.index(i)
+        f = i
+  
+print "Friend in place,",p+1," Wins with",f,"as Card Numbers"
+
