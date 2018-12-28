7ba69a9accf0ca63635c46741c66adf88aa43f12
CVAS7 <suresh.malasani@gmail.com>
2018-12-28 11:21:03 +0400

    Rename Add_Sub_Funcation_Day2.py to Add_Sub_Funcation_Day2_loop.py

---
+++ Add_Sub_Funcation_Day2_loop.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#import S0BQ01ContinueImportFuncation
+def add(n1,n2):
+    add1 = n1+n2
+    return add1
+
+def sub(n3,n4):
+    sub1 = n3-n4
+    return sub1
+
+Count1 = 10
+Count2 = 20
+Eng1 = 200
+Eng2 = 150
+Ind1 = 300
+addres = add(Eng1,Eng2)
+
+#print addres
+
+subres = sub(addres,Ind1)
+
+def IndiaToWin():
+    print "India needs",subres+1,"to win the Match"
+# Main Here
+import S0BQ01ContinueImportFuncation
+res = True
+while res == True:
+    res = S0BQ01ContinueImportFuncation.ask()
+    if res == True:
+        IndiaToWin()
+
+=============================
+=============================
+=============================
+=============================
+python Add_Sub_Funcation_Day2.py
+Do you want to Continue:
+yes
+Continue
+India needs 51 to win the Match
+Do you want to Continue:
+Yes
+Continue
+India needs 51 to win the Match
+Do you want to Continue:
+yEs
+Continue
+India needs 51 to win the Match
+Do you want to Continue:
+YES
+Continue
+India needs 51 to win the Match
+Do you want to Continue:
+N
