a4b2ee98104fe7738e735376f01b36606783258c
CVAS7 <suresh.malasani@gmail.com>
2019-01-15 15:47:11 +0400

    Create S03Q02_unitplace.py
    Ouput:
    localhost Python]$ python S03Q02_unitplace.py
    Enter a number:1
    8
    localhost Python]$ python S03Q02_unitplace.py
    Enter a number:22
    2
    localhost Python]$ python S03Q02_unitplace.py
    Enter a number:333
    Enter the 2nd Number:1
    4

---
+++ S03Q02_unitplace.py
@@ -0,0 +1,50 @@
+"""     - Ask the user to enter a number.
+            - If the number is a single digit number,
+                 add 7 to it, and print the number in its units place
+            - If the number is a two digit number,
+                raise the number to the power of 5, 
+                and print the number in its units place
+            - If the number is a three digit number, 
+                ask the user to enter another number. 
+                Add the 2 numbers and print the number in its units place
+            Use the solution provided in S03Q01 as the template for this exercises.
+            - Instead of doing a print to print the final result in "perform_check" function 
+            call separate functions : 
+               do_1digit_oper() and
+               do_2digit_oper() and
+               do_3digit_oper()
+            that will perform the required operations based on the number of digits.
+add
+unitplace
+power5
+digitcheck
+"""
+
+def addn(n1,n2=7):
+    sum1 = n1+n2
+    units(str(sum1))
+
+def units(n1):
+    print n1[-1:]
+
+def powern(n1,n2):
+    p = pow(n1,n2)
+    units(str(p))
+
+def askother(n1):
+    n2 = raw_input("Enter the 2nd Number:")
+    s=addn(n1,int(n2))
+
+def Digitscheck(n):
+    if len(n) == 1:
+        addn(int(n))
+    elif len(n) == 2:
+        powern(int(n),n2=5)
+    elif len(n) == 3:
+	askother(int(n))
+    else:
+        print n #Printing number as is
+
+#Main
+n1 = raw_input("Enter a number:")
+Digitscheck(n1)
