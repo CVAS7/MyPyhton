fd08c1e1e629af348d0e438125377f9a93351cdf
CVAS7 <suresh.malasani@gmail.com>
2019-01-17 10:20:13 +0400

    Update S05Q04ClosestFibonacciNumber.py

--- S05Q04ClosestFibonacciNumber.py
+++ S05Q04ClosestFibonacciNumber.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 """- Check if a given number is a fibonacci number. 
            If not, then print the closest fibonacci number to the given number"""
-
 n1 = 1
 n2 = 1
 fib = [n1,n2]
@@ -12,8 +11,10 @@ while (i <= number):
     n3 = n1+n2
     n1 = n2
     n2 = n3
-    fib.append(n3)
+    if n3 <= number:
+        fib.append(n3)
+print fib
 if number in fib:
     print ("Given number is fibonacci")
 else:
-    print "Given number is not fibonacci"
+    print "Given number is not fibonacci and Closest numbers is ",fib[len(fib)-1]
