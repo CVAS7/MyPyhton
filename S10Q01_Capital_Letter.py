430598003e5902cb45808d4c4c5da453dba125df
CVAS7 <suresh.malasani@gmail.com>
2019-01-25 14:54:23 +0400

    Create S10Q01_Capital_Letter.py

---
+++ S10Q01_Capital_Letter.py
@@ -0,0 +1,15 @@
+"""
+Get the name of the text file from the user.
+Check if all the sentences in that text file begin with a capital letter.
+"""
+# You have three options for punctuating the end of a sentence: a period(.) an exclamation mark(!) or a question mark(?)
+fname = raw_input("Enter the file name:")
+FH = open(fname,"r")
+x = FH.readlines()
+for i in xrange(len(x)):
+    print x[i][:1]
+    if str(x[i][:1]) == str.upper(x[i][:1]):
+        print "line:",i,"begin with a capital letter that is",str(x[i][:1])
+    else:
+        print "line:",i,"Does not begin with a capital letter that is",str(x[i][:1])
+FH.close()
